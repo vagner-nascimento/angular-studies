0a3d52b840bbe2c5ffc92b5545b852fd4824ba57
Vagner Nascimento <vagnernasc88@gmail.com>
2019-03-06 21:00:51 -0300

    Directives lesson

--- jedi-academy/src/app/app.component.ts
+++ jedi-academy/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Student } from './student/student.model';
 
 @Component({
   selector: 'jad-root', //NOTE: This annotation contain the map for the html and css page putted on browser
@@ -10,8 +11,10 @@ export class AppComponent {
 IMPORTANT NOTE: These objects are json representation of Student interface, founded in student component folder
 Remeber, you should to send boolean and numbers as it is, not strings
 */
-  luke = {name: "Luke", isJedi: true, temple: "Coruscant"}
-  han  = {name: "Han Solo", isJedi: false}
-  yoda = {name: "Yoda Master", isJedi: true, temple: "Black Kaiser" }
-  leia = {name: "Leia Princess", isJedi: false }
+  students: Student[] = [
+    {name: "Luke", isJedi: true, temple: "Coruscant"},
+    {name: "Han Solo", isJedi: false},
+    {name: "Yoda Master", isJedi: true, temple: "Black Kaiser" },
+    {name: "Leia Princess", isJedi: false }
+  ]
 }
