1da989ab4b49502e0361315f238b18d66fd68960
Vagner Nascimento <vagnernasc88@gmail.com>
2019-03-19 20:37:37 -0300

    Added restaurants components with static item and added it to site

---
+++ meat-app-starter/src/app/restaurants/restaurant/restaurant.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mt-restaurant',
+  templateUrl: './restaurant.component.html'
+})
+export class RestaurantComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
