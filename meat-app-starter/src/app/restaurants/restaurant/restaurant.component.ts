73428714acc070ca09bb58157000b1450509b209
Vagner Nascimento <vagnernasc88@gmail.com>
2019-03-19 21:07:39 -0300

    Added restaurant component to create restaurants dinamically into restaurants page

--- meat-app-starter/src/app/restaurants/restaurant/restaurant.component.ts
+++ meat-app-starter/src/app/restaurants/restaurant/restaurant.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Restaurant } from './restaurant.model';
 
 @Component({
   selector: 'mt-restaurant',
@@ -6,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RestaurantComponent implements OnInit {
 
+  @Input() restaurant: Restaurant
+
   constructor() { }
 
   ngOnInit() {
   }
+  
 }
---
+++ meat-app-starter/src/app/restaurants/restaurant/restaurant.model.ts
@@ -0,0 +1,9 @@
+export interface Restaurant {
+    //NOTES: copied from "db.json" file
+    id: string
+    name: string
+    category: string
+    deliveryEstimate: string
+    rating: number
+    imagePath: string    
+}
