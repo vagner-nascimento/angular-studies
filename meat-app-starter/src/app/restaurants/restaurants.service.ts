da71141ed70fe1a35eeda94ba9cb7b0e781fd9bf
Vagner Nascimento <vagnernasc88@gmail.com>
2019-03-27 22:00:09 -0300

    Created an error handler and called it on catch of rest call

--- meat-app-starter/src/app/restaurants/restaurants.service.ts
+++ meat-app-starter/src/app/restaurants/restaurants.service.ts
@@ -3,7 +3,10 @@ import { Restaurant } from "./restaurant/restaurant.model";
 import { Http } from "@angular/http";
 import { MEAT_API } from "app/app.api";
 import { Observable } from "rxjs/Observable";
+import { ErrorHandler } from "app/app.error-handler"
 import 'rxjs/add/operator/map' //NOTE: Is necessary to declare it manually to use ".map" to get response JSON
+import 'rxjs/add/operator/catch'
+import 'rxjs/add/observable/throw'
 
 @Injectable() //NOTE: That is necessary for angular make depency injection of Http "Rest Client" (a piece a cacke)
 export class RestautsService implements OnInit {
@@ -12,9 +15,13 @@ export class RestautsService implements OnInit {
 
     ngOnInit(): void {
     }
-
+    //NOTE: getRestaurants mothod dosen't call the api, 
+    //  it only configures the observable to who's calls him, 
+    //  so the caller does the subscrible and manipules the response as his needs
     getRestaurants(): Observable<Restaurant[]> {
         return this.http.get(`${MEAT_API}/restaurants`)
                         .map(response => response.json())
+                        //NOTE: catch is used to call a static function created in the default error handler, it pass a Response through the static function, that recieves and handles it
+                        .catch(ErrorHandler.handleError)
     }
 }
