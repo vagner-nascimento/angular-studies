35b8b2cb83d6a84653fa0d7dc30171bf46b3290c
Vagner Nascimento <vagnernasc88@gmail.com>
2019-04-25 17:39:41 -0300

    Added radio component

--- meat-app-starter/src/app/order/order.component.ts
+++ meat-app-starter/src/app/order/order.component.ts
@@ -1,10 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { RadioOption } from '../shared/radio/radio-option.model';
+
 
 @Component({
   selector: 'mt-order',
   templateUrl: './order.component.html'
 })
 export class OrderComponent implements OnInit {
+
+  paymentOtions: RadioOption[] = [
+    {label: 'Dinheiro', value: 'MON'},
+    {label: 'Cartao Débito', value: 'DEB'},
+    {label: 'Cartao Refeiçao', value: 'REF'}
+  ]
 
   constructor() { }
 
