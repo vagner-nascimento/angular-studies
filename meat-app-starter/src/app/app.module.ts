cd7c95a599fc0b6b3e754cd19c77a90cec06a699
Vagner Nascimento <vagnernasc88@gmail.com>
2019-04-29 20:13:31 -0300

    Implemented rating component with EVENT EMMITER

--- meat-app-starter/src/app/app.module.ts
+++ meat-app-starter/src/app/app.module.ts
@@ -25,6 +25,7 @@ import { OrderItemsComponent } from './order/order-items/order-items.component'
 import { OrderService } from './order/order.service';
 import { DeliveryCostsComponent } from './order/delivery-costs/delivery-costs.component';
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
+import { RatingComponent } from './shared/rating/rating.component';
 
 @NgModule({
   declarations: [
@@ -44,7 +45,8 @@ import { OrderSummaryComponent } from './order-summary/order-summary.component';
     RadioComponent,
     OrderItemsComponent,
     DeliveryCostsComponent,
-    OrderSummaryComponent
+    OrderSummaryComponent,
+    RatingComponent
   ],
   imports: [
     BrowserModule,
